4988bde3ee000d11265149091bc8c27ad8cbe14d
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-07 00:18:09 -0400

    Solution to stretch: create server from scratch

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 'use strict';
 
-const morgan = require('morgan');
 const express = require('express');
-
-const { top50Page, popularArtist, showSong, fourOhFour } =
-    require('./songHandlers.js');
-const { booksPage, showBook, showGenre } = require('./bookHandlers.js');
-
-const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.send('<h1>bacon</h1>');
+});
 
-app.get('/top50', top50Page);
-app.get('/top50/song/:pagenum', showSong);
-app.get('/top50/popular-artist', popularArtist);
-app.get('/books', booksPage);
-app.get('/books/book/:booknum', showBook);
-app.get('/books/:genre', showGenre);
-app.get('*', fourOhFour);
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(4000);
